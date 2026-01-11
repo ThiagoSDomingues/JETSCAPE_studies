2c139380268f545e67906cfcffdef674bfcb8187
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-11 12:06:13 -0300

    Update plots.py

--- model/plots.py
+++ model/plots.py
@@ -1,1 +1,30 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+# =============================================================================
+# CONFIGURATION
+# =============================================================================
+
+# Input files for different viscous corrections
+VISCOUS_CORRECTIONS = {
+    'Grad': 'design_points_data/differential_radial_flow/v0pt_design_points_results_Pb_Pb_2760_Grad.pkl',
+    'CE':   'design_points_data/differential_radial_flow/v0pt_design_points_results_Pb_Pb_2760_CE.pkl',
+    'PTM':  'design_points_data/differential_radial_flow/v0pt_design_points_results_Pb_Pb_2760_PTM.pkl',
+    'PTB':  'design_points_data/differential_radial_flow/v0pt_design_points_results_Pb_Pb_2760_PTB.pkl'
+}
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# SELECT WHICH VISCOUS CORRECTION TO PLOT
+# Options: 'Grad', 'CE', 'PTM', 'PTB'
+SELECTED_CORRECTION = 'Grad'
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# Centrality to plot (index in centbins array)
+CENTRALITY_INDEX = 6  # 50-60%
+CENTRALITY_LABEL = '50-60%'
+
+# Species to plot
+SPECIES = 'charged'  # 'charged', 'pi', 'kaon', 'proton', 'Sigma', 'Xi'
 
