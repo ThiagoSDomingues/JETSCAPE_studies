a6901e50e66bcfd976188b2677f4333819017bad
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-19 16:56:58 -0300

    Add reshaping of predictions for cent and pt bins
    Reshape predictions into specified dimensions for analysis.

--- model/emu_v0.py
+++ model/emu_v0.py
@@ -38,3 +38,10 @@ for params in np.random.uniform(param_min, param_max, (n_pr_samples, n_params)):
     y_pred, cov_pred = predict_observables(params, emulators, inv_tf, scaler)
     pr_predictions.append(y_pred.flatten())
 pr_predictions = np.array(pr_predictions) # shape (n_pr_samples, n_obs)
+print(pr_predictions.shape)
+
+# Reshape predictions into (n_pr_samples, n_cent_bins, n_pt_bins)
+pr_n_cent = 10 ### change it! 
+pr_n_pt = 29 ### change it!   
+pr_predictions_reshaped = pr_predictions.reshape(n_pr_samples, pr_n_cent, pr_n_pt)
+pr_predictions_reshaped.shape
